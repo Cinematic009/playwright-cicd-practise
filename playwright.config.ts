6e5f866fe56a599c56a87ee4f0ab18f5917a38c4
sunilfirstEdgetech <sunil.ghatul@firstedgetech.com>
2026-06-14 22:24:33 +0530

    added missing browsers firefox and webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,18 +23,18 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    //     {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
+        {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
   ],
 });
